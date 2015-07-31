32f1c19494b19aaadd946c1a5ed76ad629531c4e
Erick Chali <erickdemon@gmail.com>
2015-07-30 20:36:06 -0600

    DatosDet
    agregada la clase para detalle de producos

--- PV/src/com/im/puntoventa/conexion/ConectarDB.java
+++ PV/src/com/im/puntoventa/conexion/ConectarDB.java
@@ -1,9 +1,9 @@
 package com.im.puntoventa.conexion;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+//import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,43 +12,44 @@ public class ConectarDB {
 	
 	public Connection getConnection(){
 //		leer UDL
-		BufferedReader reader=null;
-		String[] datos = new String[14];
-		try {
-			reader = new BufferedReader(new FileReader(
-				"C:/millenium.udl"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		BufferedReader reader=null;
+//		String[] datos = new String[14];
+//		try {
+//			reader = new BufferedReader(new FileReader(
+//				"C:/millenium.udl"));
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
 		// Read lines from file.
-		while (true) {
-		    String line = null;
-			try {
-				line = reader.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    if (line == null) {
-			break;
-		    }
-		    // Split line on comma.
-		    String[] parts = line.split(";");
-		    int x = 0;
-		    for (String part : parts) {
-		    	datos[x] = part;
-		    	x++;
-		    }
-		}
+//		while (true) {
+//		    String line = null;
+//			try {
+//				line = reader.readLine();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		    if (line == null) {
+//			break;
+//		    }
+//		    // Split line on comma.
+//		    String[] parts = line.split(";");
+//		    int x = 0;
+//		    for (String part : parts) {
+//		    	datos[x] = part;
+//		    	x++;
+//		    }
+//		}
 		
-		try {
-			reader.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			reader.close();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 //		leer UDL
 		Connection conectar = null;
 		try {
@@ -64,7 +65,7 @@ public class ConectarDB {
 //			String usr = datos[3];
 //			String pass = datos[4];
 //			conectar = DriverManager.getConnection("jdbc:sqlserver://186.64.110.212;databaseName=PbMillenium2;"+ datos[3] + ";" + datos[1] + ";");
-			conectar = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=MilleniumNew;user=sa;password=SOLUCION");
+			conectar = DriverManager.getConnection("jdbc:sqlserver://vmsoluciones.cloudapp.net;databaseName=MilleniumNew;user=sa;password=SOLUCION");
 //			conectar = DriverManager.getConnection("jdbc:sqlserver://186.64.110.212;databaseName=PbMillenium2;"+datos[3]+";"+datos[1]+";"+"integratedSecurity=true");
 //			conectar = DriverManager.getConnection("jdbc:sqlserver://186.64.110.212;databaseName=PbMillenium2;user=consultas;password=consultas*");
 		} catch (SQLException sqlex) {
---
+++ PV/src/com/im/puntoventa/dao/InterfazDatosDet.java
@@ -0,0 +1,5 @@
+package com.im.puntoventa.dao;
+
+public class InterfazDatosDet {
+
+}
